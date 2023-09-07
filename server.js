a642ea8b626e708798f4a83960422a14a2b2c609
eliasgebreamanuel <eliasgamanuel@gmail.com>
2023-09-07 07:02:42 +0300

    second commit

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const http = require('http')
 const fs = require('fs')
+const _ = require('lodash')
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
-    // these are the three steps to send a response
+   // console.log(req.url, req.method)
+    
+   // this is for the lodash
+   const num = _.random(0,20000000)
+
+
+   // this is used ot execte a fucntion only once
+   const greet = _.once(() => {
+    console.log('Hello')
+   })
+   greet();
+   greet();
+   console.log(num)
+   // these are the three steps to send a response
     // sdet header content type
     res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url){
+        case  '/':
+            path +='index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += "about.html"
+            res.statusCode = 200
+            break
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break
+        default: 
+            path +='404.html'
+            res.statusCode = 404
+            break
+
+    }
     // res.write('<head><link rel = "stylesheet" href="#"></head>')
     // res.write('<h1>Hello Elias</h1>')
     // res.write('<h1>Hello Elias Again</h1>')
     // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err)
             res.end()
